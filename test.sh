e00a05ec6c69b593bde865e066f671d707f5f4bf
Zoltan Nagy <abesto0@gmail.com>
2022-10-02 17:23:16 +0000

    Chore: updated test.sh; still no relevant tests though

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 cargo build
 cd ../craftinginterpreters
-dart ./tool/bin/test.dart clox chap14_chunks --interpreter ../clox-rs/target/debug/clox-rs
+dart ./tool/bin/test.dart clox chap15_virtual --interpreter ../clox-rs/target/debug/clox-rs
